a84ab882a130f623f00ac23257a6093393f4c48b
Tim Conkling <tconkling@gmail.com>
2011-08-15 12:53:39 -0700

    Unused import

--- src/test/java/com/threerings/flashbang/PlaceholderTest.java
+++ src/test/java/com/threerings/flashbang/PlaceholderTest.java
@@ -7,8 +7,6 @@ package com.threerings.flashbang;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class PlaceholderTest
 {
     @Test public void placeholderTest () {
