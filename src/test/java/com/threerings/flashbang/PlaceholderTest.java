c7eaa5c93cabccb5ce1b73d57b39d27c45200160
Tim Conkling <tconkling@gmail.com>
2011-08-15 12:46:05 -0700

    a placeholder unit test

---
+++ src/test/java/com/threerings/flashbang/PlaceholderTest.java
@@ -0,0 +1,16 @@
+//
+// Flashbang - a framework for creating PlayN games
+// Copyright (C) 2011 Three Rings Design, Inc., All Rights Reserved
+// http://github.com/threerings/flashbang-playn
+
+package com.threerings.flashbang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaceholderTest
+{
+    @Test public void placeholderTest () {
+    }
+}
