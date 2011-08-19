ac0093d400cdaccf7abc8cc85e4526b594908c36
Tim Conkling <tconkling@gmail.com>
2011-08-19 15:56:10 -0700

    ResourceManager, ResourceSet

---
+++ src/main/java/com/threerings/flashbang/rsrc/ResourceManager.java
@@ -0,0 +1,84 @@
+//
+// Flashbang - a framework for creating PlayN games
+// Copyright (C) 2011 Three Rings Design, Inc., All Rights Reserved
+// http://github.com/threerings/flashbang-playn
+
+package com.threerings.flashbang.rsrc;
+
+import java.util.Map;
+
+import playn.core.Image;
+import playn.core.Json;
+import playn.core.Sound;
+
+import com.google.common.collect.Maps;
+import com.google.gwt.thirdparty.guava.common.base.Preconditions;
+
+public class ResourceManager
+{
+    public Resource<?> getResource (String name)
+    {
+        return _resources.get(name);
+    }
+
+    public Sound getSound (String name)
+    {
+        Resource<?> rsrc = getResource(name);
+        if (rsrc == null) {
+            return null;
+        }
+        Preconditions.checkState(rsrc instanceof SoundResource);
+        return ((SoundResource) rsrc).get();
+    }
+
+    public Image getImage (String name)
+    {
+        Resource<?> rsrc = getResource(name);
+        if (rsrc == null) {
+            return null;
+        }
+        Preconditions.checkState(rsrc instanceof ImageResource);
+        return ((ImageResource) rsrc).get();
+    }
+
+    public Json.Object getJson (String name)
+    {
+        Resource<?> rsrc = getResource(name);
+        if (rsrc == null) {
+            return null;
+        }
+        Preconditions.checkState(rsrc instanceof JsonResource);
+        return ((JsonResource) rsrc).get();
+    }
+
+    public String getText (String name)
+    {
+        Resource<?> rsrc = getResource(name);
+        if (rsrc == null) {
+            return null;
+        }
+        Preconditions.checkState(rsrc instanceof TextResource);
+        return ((TextResource) rsrc).get();
+    }
+
+    public boolean isResourceLoaded (String name)
+    {
+        return (getResource(name) != null);
+    }
+
+    public void unloadAll ()
+    {
+        _resources.clear();
+    }
+
+    void addAll (Iterable<Resource<?>> resources)
+    {
+        for (Resource<?> rsrc : resources) {
+            Resource<?> old = _resources.put(rsrc.path, rsrc);
+            Preconditions.checkState(old == null,
+                "A resource with that name already exists [name=%s]", rsrc.path);
+        }
+    }
+
+    protected Map<String, Resource<?>> _resources = Maps.newHashMap();
+}
---
+++ src/main/java/com/threerings/flashbang/rsrc/ResourceSet.java
@@ -0,0 +1,62 @@
+//
+// Flashbang - a framework for creating PlayN games
+// Copyright (C) 2011 Three Rings Design, Inc., All Rights Reserved
+// http://github.com/threerings/flashbang-playn
+
+package com.threerings.flashbang.rsrc;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+import com.google.gwt.thirdparty.guava.common.base.Preconditions;
+
+import com.threerings.flashbang.util.Loadable;
+import com.threerings.flashbang.util.LoadableBatch;
+
+public class ResourceSet extends LoadableBatch
+{
+    public ResourceSet (ResourceManager rm, boolean loadInSequence)
+    {
+        super(loadInSequence);
+        _rm = rm;
+    }
+
+    public ResourceSet (ResourceManager rm)
+    {
+        this(rm, false);
+    }
+
+    @Override
+    public void addLoadable (Loadable loadable)
+    {
+        if (loadable instanceof Resource) {
+            Resource<?> rsrc = (Resource<?>) loadable;
+            Resource<?> old = _resources.put(rsrc.path, rsrc);
+            Preconditions.checkState(old == null,
+                "A resource with that name is already queued [name=%s]", rsrc.path);
+        }
+        super.addLoadable(loadable);
+    }
+
+    public boolean containsResource (String name)
+    {
+        return _resources.containsKey(name);
+    }
+
+    @Override
+    protected void loadComplete (Throwable err)
+    {
+        if (err == null) {
+            try {
+                _rm.addAll(_resources.values());
+            } catch (Throwable addErr) {
+                err = addErr;
+            }
+        }
+
+        super.loadComplete(err);
+    }
+
+    protected final ResourceManager _rm;
+    protected Map<String, Resource<?>> _resources = Maps.newHashMap();
+}
