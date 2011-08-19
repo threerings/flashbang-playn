e439816664f52ae59a47abbe6060e63a7365803a
Tim Conkling <tconkling@gmail.com>
2011-08-19 15:54:56 -0700

    JsonResource

---
+++ src/main/java/com/threerings/flashbang/rsrc/JsonResource.java
@@ -0,0 +1,44 @@
+//
+// Flashbang - a framework for creating PlayN games
+// Copyright (C) 2011 Three Rings Design, Inc., All Rights Reserved
+// http://github.com/threerings/flashbang-playn
+
+package com.threerings.flashbang.rsrc;
+
+import playn.core.Json;
+import playn.core.PlayN;
+import playn.core.ResourceCallback;
+
+public class JsonResource extends Resource<Json.Object>
+{
+    public JsonResource (String path)
+    {
+        super(path);
+    }
+
+    @Override
+    protected void doLoad ()
+    {
+        PlayN.assetManager().getText(path, new ResourceCallback<String>() {
+            @Override public void done (String text) {
+                try {
+                    _json = PlayN.json().parse(text);
+                } catch (Throwable err) {
+                    loadComplete(err);
+                }
+                loadComplete(null);
+            }
+            @Override public void error (Throwable err) {
+                loadComplete(err);
+            }
+        });
+    }
+
+    @Override
+    public Json.Object get ()
+    {
+        return _json;
+    }
+
+    protected Json.Object _json;
+}
