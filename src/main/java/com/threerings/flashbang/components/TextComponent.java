cbe25a088c3c406e3472c00fed9fd61f50013102
Tim Conkling <tconkling@gmail.com>
2011-09-03 17:51:16 -0700

    TextSceneObject, a SceneObject that handles text rendering

---
+++ src/main/java/com/threerings/flashbang/components/TextComponent.java
@@ -0,0 +1,10 @@
+//
+// Nod - Copyright 2011 Three Rings Design, Inc.
+
+package com.threerings.flashbang.components;
+
+public interface TextComponent
+{
+    String text ();
+    void setText (String text);
+}
