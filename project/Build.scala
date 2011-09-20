af9876f9eb25d032a54e8084208e6b7f217ca8f6
Michael Bayne <mdb@samskivert.com>
2011-09-20 09:23:25 -0700

    Use published sbt-condep-plugin.

--- project/Build.scala
+++ project/Build.scala
@@ -1,21 +1,8 @@
 import sbt._
 import Keys._
 
-// allows projects to be symlinked into the current directory for a direct dependency,
-// or fall back to obtaining the project from Maven otherwise
-class Locals (locals :(String, String, ModuleID)*) {
-  def addDeps (p :Project) = (locals collect {
-    case (id, subp, dep) if (file(id).exists) => symproj(file(id), subp)
-  }).foldLeft(p) { _ dependsOn _ }
-  def libDeps = locals collect {
-    case (id, subp, dep) if (!file(id).exists) => dep
-  }
-  private def symproj (dir :File, subproj :String = null) =
-    if (subproj == null) RootProject(dir) else ProjectRef(dir, subproj)
-}
-
 object FlashbangBuild extends Build {
-  val locals = new Locals(
+  val locals = com.samskivert.condep.Depends(
     ("tripleplay", null, "com.threerings" % "tripleplay" % "1.0-SNAPSHOT")
   )
 
