4d0d041ae8ca121e215dec4e445521deb497f7ae
Charlie Groves <charlie.groves@gmail.com>
2011-09-23 16:57:34 -0700

    Add a display name for keyframe types

--- src/main/java/flashbang/anim/rsrc/KeyframeType.java
+++ src/main/java/flashbang/anim/rsrc/KeyframeType.java
@@ -7,11 +7,14 @@ package flashbang.anim.rsrc;
 
 public enum KeyframeType
 {
-    X_LOCATION(0), Y_LOCATION(0), X_SCALE(1), Y_SCALE(1), ROTATION(0), ALPHA(1);
+    X_LOCATION("X", 0), Y_LOCATION("Y", 0), X_SCALE("X Scale", 1), Y_SCALE("Y Scale", 1),
+        ROTATION("Rotation", 0), ALPHA("Alpha", 1);
 
+    public final String displayName;
     public final float defaultValue;
 
-    private KeyframeType (float defaultValue) {
+    private KeyframeType (String displayName, float defaultValue) {
+        this.displayName = displayName;
         this.defaultValue = defaultValue;
     }
 }
