5b6c503ae31c30a197a70c8dfcd19a39b2bcab47
Michael Bayne <mdb@samskivert.com>
2011-11-09 13:52:22 -0800

    Update the SBT build to match the POM.

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object FlashbangBuild extends Build {
   val locals = com.samskivert.condep.Depends(
-    ("tripleplay", null, "com.threerings" % "tripleplay" % "1.0-SNAPSHOT")
+    ("tripleplay", null, "com.threerings" % "tripleplay" % "1.1-SNAPSHOT")
   )
 
   lazy val flashbang = locals.addDeps(Project(
@@ -12,7 +12,7 @@ object FlashbangBuild extends Build {
       version      := "1.0-SNAPSHOT",
       name         := "flashbang-playn",
       crossPaths   := false,
-      scalaVersion := "2.9.0-1",
+      scalaVersion := "2.9.1",
 
       javacOptions ++= Seq("-Xlint", "-Xlint:-serial", "-source", "1.6", "-target", "1.6"),
       fork in Compile := true,
